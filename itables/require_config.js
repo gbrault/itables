e9f9d3be13efd562d8451bc115971e3ff81ea719
Gilbert Brault <gilbert.brault@hotmail.com>
2022-06-04 15:09:37 +0200

    Update require_config.js

--- itables/require_config.js
+++ itables/require_config.js
@@ -1,18 +1,19 @@
-if (typeof require !== 'undefined')
+if (typeof require !== 'undefined'){
     require.config({
         paths: {
             jquery: 'https://code.jquery.com/jquery-3.5.1.min',
             datatables: 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min',
         }
     });
-console.log("Set New Table creation Hook");
-var jq = "jquery";
-var dt = "datatables";
-require([jq], function(jqr){
-    require([dt], function(dtr){
-        jqr(document).on( 'init.dt', function ( e, settings ) {
-                    var api = new jqr.fn.dataTable.Api( settings );
-                    console.log( 'Settings:',settings,'\nNew DataTable created:', api.table().node() );
-        } );
+    console.log("Set New Table creation Hook");
+    var jq = "jquery";
+    var dt = "datatables";
+    require([jq], function(jqr){
+        require([dt], function(dtr){
+            jqr(document).on( 'init.dt', function ( e, settings ) {
+                        var api = new jqr.fn.dataTable.Api( settings );
+                        console.log( 'Settings:',settings,'\nNew DataTable created:', api.table().node() );
+            } );
+        });
     });
-});
+}
