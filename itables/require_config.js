9a67fcf22c2cc5b56c0170d084968c0974cec598
Gilbert Brault <gilbert.brault@hotmail.com>
2022-06-04 10:25:56 +0200

    Update require_config.js

--- itables/require_config.js
+++ itables/require_config.js
@@ -5,3 +5,13 @@ if (typeof require !== 'undefined')
             datatables: 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min',
         }
     });
+var jq = "jquery";
+var dt = "datatables";
+require([jq], function(jqr){
+    require([dt], function(dtr){
+        jqr(document).on( 'init.dt', function ( e, settings ) {
+                    var api = new jqr.fn.dataTable.Api( settings );
+                    console.log( 'New DataTable created:', api.table().node() );
+        } );
+    });
+});
