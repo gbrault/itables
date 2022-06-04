aa7f48262a72cb28638f1ba1ad74086ebced0ae4
Gilbert Brault <gilbert.brault@hotmail.com>
2022-06-04 09:58:06 +0200

    Update require_config.js

--- itables/require_config.js
+++ itables/require_config.js
@@ -5,6 +5,7 @@ if (typeof require !== 'undefined')
             datatables: 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min',
         }
     });
+console.log("new table created hook");
 $(document).on( 'init.dt', function ( e, settings ) {
     var api = new $.fn.dataTable.Api( settings );
     console.log( 'New DataTable created:', api.table().node() );
